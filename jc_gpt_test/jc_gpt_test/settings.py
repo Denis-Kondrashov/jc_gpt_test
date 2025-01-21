8df27d58bdfc1e08a3afd61a8256807462718d9d
DenisK5 <denis.kondrashov1@gmail.com>
2025-01-21 18:37:56 +0300

    feat: Dockerfile update

--- jc_gpt_test/jc_gpt_test/settings.py
+++ jc_gpt_test/jc_gpt_test/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'jc_gpt_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
@@ -106,14 +106,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/jc_gpt_test/static/'
+# STATIC_ROOT = '/var/www/jc_gpt_test/static/'
+STATIC_ROOT = '/static/'
+
 
 
 #STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 
